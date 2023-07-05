c4a1123518007d68fdc546be2e80a5a528b067a0
Mateo <mateodinu02@gmail.com>
2023-07-05 13:12:49 -0300

    elimine las clases del otro proyecto

--- app/src/main/java/ar/edu/unlam/mobile2/MainModule.kt
+++ app/src/main/java/ar/edu/unlam/mobile2/MainModule.kt
@@ -2,8 +2,6 @@ package ar.edu.unlam.mobile2
 
 import ar.edu.unlam.mobile2.data.CountriesRepo
 import ar.edu.unlam.mobile2.data.CountriesRestRepo
-import ar.edu.unlam.mobile2.data.KittiesRepo
-import ar.edu.unlam.mobile2.data.KittiesRestRepo
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -13,8 +11,7 @@ import dagger.hilt.android.components.ViewModelComponent
 @InstallIn(ViewModelComponent::class)
 abstract class MainModule {
 
-    @Binds
-    abstract fun bindKittiesRepo(kittiesRestRepo: KittiesRestRepo): KittiesRepo
+
 
     @Binds
     abstract fun bindCountriesRepo(countriesRestRepo: CountriesRestRepo): CountriesRepo
